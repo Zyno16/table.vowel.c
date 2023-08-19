90dead4c1d8b940f72e701e9c207ca44c0dd8aae
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-08-19 06:45:47 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int i;
+
+    char t[6];
+    t[0]='A';
+   t[1]='E';
+    t[2]='I';
+    t[3]='O';
+    t[4]='U';
+    t[5]='Y';
+    for(i=0;i<6;i++){
+
+    printf("%c", t[i]);}
+    return 0;
+}
